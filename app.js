3ea653f6f057ec9440afa5ea171e52e881f8721e
Your Name <you@example.com>
2020-12-11 19:20:00 +0200

    werewr

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 /*@ here we include express-framework @*/
 
+/*@ here we include express-framework @*/
+const cors = require('cors');
+app.use(cors());
+/*@ here we include express-framework @*/
+
 /*@ here we include Socket.io @*/
 const http = require("http").Server(app);
 const io = require('socket.io')(http);
@@ -21,11 +26,10 @@ io.on('connection', (socket) => {
        }
     }); 
 
-    // socket.on('tableOnline', (table) => {
-       
-    // })
-
-    io.emit('tableOnline', { number: 5, status: 'offline' });
+    socket.on('tableOnline', (data) => {
+        io.to('1fe35579-5ce7-46ec-89e0-7e7236700297').emit('tableOnline', data);
+    })
+   
     
     
 
@@ -48,9 +52,9 @@ io.on('connection', (socket) => {
 });
 /*@ Socket.io Connection @*/
 
-/*@ NodeJS App is listening to Port-8080 @*/
-const port = process.env.PORT || 8080;
+/*@ NodeJS App is listening to Port-3000 @*/
+const port = process.env.PORT || 3000;
 http.listen(port, () => {
     console.log(`Running on Port: ${ port }`);
 });
-/*@ NodeJS App is listening to Port-8080 @*/
+/*@ NodeJS App is listening to Port-3000 @*/
